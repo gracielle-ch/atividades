1ea0a1d119d4882377a0ddd42599dc7dcdb1e637
gracielle-ch <59579212+gracielle-ch@users.noreply.github.com>
2020-01-09 09:25:30 -0300

    Update ativ3.py

--- ativ3.py
+++ ativ3.py
@@ -3,6 +3,6 @@ horas = int(input('digite com as horas: '))
 minutos = int(input('digite os minutos: '))
 segundos = int(input('digite os segundos: '))
 
-
+###teste 
 segundosF= (dia*24*60*60) + (horas*60*60) + (minutos*60) + segundos
 print ('Numero em segundos é ', segundosF)
